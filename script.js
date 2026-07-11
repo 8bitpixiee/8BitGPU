9e56c0bbf16d32cd177fd53091c611d1c6372578
8bitpixiee <villainerafit@gmail.com>
2026-07-11 01:23:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function setHair(choice) {
-  const hairLayer=document.getElementById("hairLayer");
-  if (choice==="none" {
-    hairLayer.style.display="none";
-  }
-  else {
-    hairLayer.style.display="block";
-    hairLayer.src=choice;
+  const hairLayer = document.getElemtById(hairLayer");
+
+if (choice === "none") {
+  hairLayer.style.display = "none";
+} else {
+  hairLayer.style.display = "block";
+  hairLayer.src = choice;
+}
+}
+function toggleMenu() {
+  const menu = document.getElementById("avatarMenu");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
   }
 }
-function toggleMenu {
-  display: none;
-}
-alert ("script workin");
