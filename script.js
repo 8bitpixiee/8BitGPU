439f568e8b0cccf226e7ec4fc8783267d6900020
8bitpixiee <villainerafit@gmail.com>
2026-07-14 10:41:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 function toggleMenu() {
-    alert("CLICK WORKS");
-
     const menu = document.getElementById("avatarMenu");
 
-    alert(menu);
+    menu.style.display =
+        menu.style.display === "flex"
+            ? "none"
+            : "flex";
+}
 
-    if (menu.style.display === "none") {
-        menu.style.display = "flex";
+function setHair(choice) {
+    const hairLayer = document.getElementById("hairLayer");
+
+    if (choice === "none") {
+        hairLayer.style.display = "none";
     } else {
-        menu.style.display = "none";
+        hairLayer.style.display = "block";
+        hairLayer.src = choice;
     }
 }
+
+document.addEventListener("click", function(event) {
+    const menu = document.getElementById("avatarMenu");
+    const companion = document.getElementById("companion");
+
+    if (
+        !menu.contains(event.target) &&
+        !companion.contains(event.target)
+    ) {
+        menu.style.display = "none";
+    }
+});
