3fc1a3b96cf5c5cb0a81ebdeeffcbec4402aa7d7
8bitpixiee <villainerafit@gmail.com>
2026-07-16 18:04:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function toggleMenu() {
-    const menu = document.getElementById("avatarMenu");
-
-    menu.style.display =
-        menu.style.display === "flex"
-            ? "none"
-            : "flex";
-}
-
-function setHair(choice) {
-    const hairLayer = document.getElementById("hairLayer");
-
-    if (choice === "none") {
-        hairLayer.style.display = "none";
-    } else {
-        hairLayer.style.display = "block";
-        hairLayer.src = choice;
-    }
-}
-
 function loadSavedOutfit() {
     let savedOutfit;
 
@@ -65,20 +45,6 @@ function loadSavedOutfit() {
     setLayer("fitLayer", fitOptions[savedOutfit.fit] || "");
     setLayer("extraLayer", extraOptions[savedOutfit.extra] || "");
 
-    const menu = document.getElementById("avatarMenu");
-    menu.style.display = "none";
 }
-
-document.addEventListener("click", function(event) {
-    const menu = document.getElementById("avatarMenu");
-    const companion = document.getElementById("companion");
-
-    if (
-        !menu.contains(event.target) &&
-        !companion.contains(event.target)
-    ) {
-        menu.style.display = "none";
-    }
-});
 
 loadSavedOutfit();
